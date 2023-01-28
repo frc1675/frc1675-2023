c0220788851b17adc1312c665976b39601d7bab0
python-42 <herrmann.jake@protonmail.com>
2023-01-28 16:00:28 -0600

    Added swerve deps and code

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,52 +4,44 @@
 
 package frc.robot;
 
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
-import frc.robot.Constants.OperatorConstants;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import frc.robot.commands.DefaultDriveCommand;
+import frc.robot.commands.RotateCommand;
+import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.Vision;
+import frc.robot.util.JoystickModification;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
   private final Vision vision = new Vision();
+  private final DrivetrainSubsystem drivetrainSubsystem = new DrivetrainSubsystem();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
-  private final CommandXboxController driverController =
-      new CommandXboxController(OperatorConstants.kDriverControllerPort);
+  private final Joystick driverController = new Joystick(Constants.DRIVER_CONTROLLER);
+  private final JoystickButton aButton = new JoystickButton(driverController, Constants.A_BUTTON);
+  private final JoystickButton backButton = new JoystickButton(driverController, Constants.BACK_BUTTON);
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
     configureBindings();
   }
 
-  /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
-   * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
-   * joysticks}.
-   */
   private void configureBindings() {
+    drivetrainSubsystem.setDefaultCommand(new DefaultDriveCommand(
+        drivetrainSubsystem,
+        () -> -JoystickModification.modifyAxis(driverController.getRawAxis(Constants.LEFT_Y_AXIS))
+            * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        () -> -JoystickModification.modifyAxis(driverController.getRawAxis(Constants.LEFT_X_AXIS))
+            * DrivetrainSubsystem.MAX_VELOCITY_METERS_PER_SECOND,
+        () -> -JoystickModification.modifyAxis(driverController.getRawAxis(Constants.RIGHT_X_AXIS))
+            * DrivetrainSubsystem.MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND));
 
+    backButton.onTrue(new InstantCommand(drivetrainSubsystem::zeroGyroscope));
+    aButton.whileTrue(new RotateCommand(drivetrainSubsystem, new Rotation2d(0)));
   }
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
   public Command getAutonomousCommand() {
-    // An example command will be run in autonomous
     return null;
   }
 }
---
+++ src/main/java/frc/robot/subsystems/DrivetrainSubsystem.java
@@ -0,0 +1,136 @@
+package frc.robot.subsystems;
+
+import static frc.robot.Constants.*;
+import com.kauailabs.navx.frc.AHRS;
+import com.swervedrivespecialties.swervelib.Mk4SwerveModuleHelper;
+import com.swervedrivespecialties.swervelib.SdsModuleConfigurations;
+import com.swervedrivespecialties.swervelib.SwerveModule;
+
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.networktables.DoublePublisher;
+import edu.wpi.first.networktables.DoubleTopic;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.shuffleboard.BuiltInLayouts;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class DrivetrainSubsystem extends SubsystemBase {
+        public static final double MAX_VOLTAGE = 12;
+        public static final double MAX_VELOCITY_METERS_PER_SECOND = 6380.0 / 60.0 *
+                        SdsModuleConfigurations.MK4_L3.getDriveReduction() *
+                        SdsModuleConfigurations.MK4_L3.getWheelDiameter() * Math.PI;
+        public static final double MAX_ANGULAR_VELOCITY_RADIANS_PER_SECOND = MAX_VELOCITY_METERS_PER_SECOND /
+                        Math.hypot(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0);
+
+        private final SwerveDriveKinematics m_kinematics = new SwerveDriveKinematics(
+                        // Front left
+                        new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+                        // Front right
+                        new Translation2d(DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0),
+                        // Back left
+                        new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, DRIVETRAIN_WHEELBASE_METERS / 2.0),
+                        // Back right
+                        new Translation2d(-DRIVETRAIN_TRACKWIDTH_METERS / 2.0, -DRIVETRAIN_WHEELBASE_METERS / 2.0));
+
+        private final AHRS navx = new AHRS(SPI.Port.kMXP, (byte) 200);
+
+        private final SwerveModule frontLeftModule;
+        private final SwerveModule frontRightModule;
+        private final SwerveModule backLeftModule;
+        private final SwerveModule backRightModule;
+
+        private NetworkTable table = NetworkTableInstance.getDefault().getTable("Drivetrain");
+        private DoubleTopic topic = table.getDoubleTopic("Gyro Rotation");
+        private DoublePublisher rotation = topic.publish();
+
+        private ChassisSpeeds chassisSpeeds = new ChassisSpeeds(0.0, 0.0, 0.0);
+
+        public DrivetrainSubsystem() {
+                ShuffleboardTab tab = Shuffleboard.getTab("Drivetrain");
+
+                frontLeftModule = Mk4SwerveModuleHelper.createNeo(
+                                tab.getLayout("Front Left Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(0, 0),
+                                Mk4SwerveModuleHelper.GearRatio.L3,
+                                FRONT_LEFT_MODULE_DRIVE_MOTOR,
+                                FRONT_LEFT_MODULE_STEER_MOTOR,
+                                FRONT_LEFT_MODULE_STEER_ENCODER,
+                                FRONT_LEFT_MODULE_STEER_OFFSET);
+
+                frontRightModule = Mk4SwerveModuleHelper.createNeo(
+                                tab.getLayout("Front Right Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(2, 0),
+                                Mk4SwerveModuleHelper.GearRatio.L3,
+                                FRONT_RIGHT_MODULE_DRIVE_MOTOR,
+                                FRONT_RIGHT_MODULE_STEER_MOTOR,
+                                FRONT_RIGHT_MODULE_STEER_ENCODER,
+                                FRONT_RIGHT_MODULE_STEER_OFFSET);
+
+                backLeftModule = Mk4SwerveModuleHelper.createNeo(
+                                tab.getLayout("Back Left Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(4, 0),
+                                Mk4SwerveModuleHelper.GearRatio.L3,
+                                BACK_LEFT_MODULE_DRIVE_MOTOR,
+                                BACK_LEFT_MODULE_STEER_MOTOR,
+                                BACK_LEFT_MODULE_STEER_ENCODER,
+                                BACK_LEFT_MODULE_STEER_OFFSET);
+
+                backRightModule = Mk4SwerveModuleHelper.createNeo(
+                                tab.getLayout("Back Right Module", BuiltInLayouts.kList)
+                                                .withSize(2, 4)
+                                                .withPosition(6, 0),
+                                Mk4SwerveModuleHelper.GearRatio.L3,
+                                BACK_RIGHT_MODULE_DRIVE_MOTOR,
+                                BACK_RIGHT_MODULE_STEER_MOTOR,
+                                BACK_RIGHT_MODULE_STEER_ENCODER,
+                                BACK_RIGHT_MODULE_STEER_OFFSET);
+        }
+
+        public void zeroGyroscope() {
+                navx.zeroYaw();
+        }
+
+        public Rotation2d getGyroscopeRotation() {
+
+                if (navx.isMagnetometerCalibrated()) {
+                        return Rotation2d.fromDegrees(navx.getFusedHeading());
+                }
+
+                return Rotation2d.fromDegrees(360.0 - navx.getYaw());
+        }
+
+        public void drive(ChassisSpeeds chassisSpeeds) {
+                this.chassisSpeeds = chassisSpeeds;
+        }
+
+        @Override
+        public void periodic() {
+                SwerveModuleState[] states = m_kinematics.toSwerveModuleStates(chassisSpeeds);
+                SwerveDriveKinematics.desaturateWheelSpeeds(states, MAX_VELOCITY_METERS_PER_SECOND);
+
+                frontLeftModule.set(states[0].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[0].angle.getRadians());
+                frontRightModule.set(states[1].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[1].angle.getRadians());
+                backLeftModule.set(states[2].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[2].angle.getRadians());
+                backRightModule.set(states[3].speedMetersPerSecond / MAX_VELOCITY_METERS_PER_SECOND * MAX_VOLTAGE,
+                                states[3].angle.getRadians());
+
+                rotation.set(getGyroscopeRotation().getDegrees());
+        }
+
+        public void rotate(double radiansPerSecond) {
+                drive(new ChassisSpeeds(0, 0, radiansPerSecond));
+        }
+}
