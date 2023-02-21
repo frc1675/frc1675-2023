08645d1a312426c8477d6ed25636999f0e8c84cd
Iron Golem <frc1675@gmail.com>
2023-02-20 20:02:00 -0600

    refactored code

--- src/main/java/frc/robot/commands/drive/DefaultDriveUpdatePose.java
+++ src/main/java/frc/robot/commands/drive/DefaultDriveUpdatePose.java
@@ -3,6 +3,7 @@ package frc.robot.commands.drive;
 import java.util.function.DoubleSupplier;
 
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import frc.robot.commands.vision.VisionPoseUpdate;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.Vision;
 
