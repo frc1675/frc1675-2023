b250535997bddc2f01117e9b0499a74a068442ee
python-42 <herrmann.jake@protonmail.com>
2023-02-22 00:41:13 -0600

    Remeasured offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -23,22 +23,22 @@ public final class Constants {
       public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
       public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
       public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; 
-      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(51.24);
   
       public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
       public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
       public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10; 
-      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(341.10);
   
       public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; 
       public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
       public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; 
-      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(32.08);
   
       public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
       public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
       public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; 
-      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(0.0);
+      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(221.86);
   
       //controller constants
       public static final int DRIVER_CONTROLLER = 0;
