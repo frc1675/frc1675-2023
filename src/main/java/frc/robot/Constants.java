8e8bd9152d603c5207bbc81dd035e63af4732239
Black Hole <ups1675@gmail.com>
2023-03-02 11:44:21 -0600

    fixed joysticks and updated swerve encoder offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -29,22 +29,22 @@ public final class Constants {
       public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
       public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
       public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; 
-      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(51.15);
+      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(39.81);
   
       public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
       public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
       public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10; 
-      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(341.28);
+      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(209.18);
   
       public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; 
       public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
       public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; 
-      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(33.19);
+      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(14.15);
   
       public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
       public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
       public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; 
-      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(221.84);
+      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(289.59);
   
       //controller constants
       public static final int DRIVER_CONTROLLER = 0;
