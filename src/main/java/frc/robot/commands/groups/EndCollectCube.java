06dc544d399d03c3d5eec6d4c33b09211cebccd3
Jake Herrmann <herrmann.jake@protonmail.com>
2023-03-05 21:31:24 -0600

    Cube collection for auto now sets intake speed

--- src/main/java/frc/robot/commands/groups/EndCollectCube.java
+++ src/main/java/frc/robot/commands/groups/EndCollectCube.java
@@ -4,6 +4,7 @@ import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Constants;
 import frc.robot.commands.floorArm.FloorMoveArmToPostion;
+import frc.robot.commands.intake.floor.FloorSetSpeed;
 import frc.robot.subsystems.FloorArmSubsystem;
 import frc.robot.subsystems.FloorIntake;
 
@@ -11,6 +12,7 @@ public class EndCollectCube extends SequentialCommandGroup {
 
   public EndCollectCube(FloorArmSubsystem floorArm, FloorIntake intake) {
     addCommands(
+      new FloorSetSpeed(intake, 0),
       new FloorMoveArmToPostion(floorArm, Constants.FLOOR_ARM_INSIDE_ROBOT_POSITION),
       new WaitCommand(0.5)
     );
