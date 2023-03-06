aeb0d0b87db2831aada73f8cb08215ca641b4a59
Jake Herrmann <herrmann.jake@protonmail.com>
2023-03-05 21:05:32 -0600

    Auto selection uses enums, Field2d auto visualizer ...on shuffleboard

--- src/main/java/frc/robot/util/AutoGenerator.java
+++ src/main/java/frc/robot/util/AutoGenerator.java
@@ -1,6 +1,7 @@
 package frc.robot.util;
 
 import java.util.HashMap;
+import java.util.Map;
 
 import com.pathplanner.lib.PathConstraints;
 import com.pathplanner.lib.PathPlanner;
@@ -9,9 +10,9 @@ import com.pathplanner.lib.auto.PIDConstants;
 import com.pathplanner.lib.auto.SwerveAutoBuilder;
 
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
-import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
+import edu.wpi.first.wpilibj.smartdashboard.Field2d;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.PrintCommand;
@@ -37,12 +38,18 @@ public class AutoGenerator {
     private SendableChooser<StartLocation> locationSelector = new SendableChooser<StartLocation>();
 
     private ShuffleboardTab autoTab = Shuffleboard.getTab("Auto");
+    private Field2d field = new Field2d();
 
     public enum AutoActions {
-        SCORE_EXIT_BALANCE,
-        SCORE_CONE_AND_EXIT,
-        SCORE_CUBE_AND_EXIT,
-        SCORE_LOW
+        SCORE_EXIT_BALANCE("ExitAndBalance"),
+        SCORE_CONE_AND_EXIT("ScoreConeAndExit"),
+        SCORE_CUBE_AND_EXIT("ScoreCubeAndExit"),
+        SCORE_LOW("ScoreLowAndExit");
+
+        public final String value;
+        private AutoActions(String value) {
+            this.value = value + " A";
+        }
     } 
 
     public enum StartLocation {
@@ -78,75 +85,31 @@ public class AutoGenerator {
         );
 
         locationSelector.setDefaultOption("Area One", StartLocation.ONE); 
-        locationSelector.addOption("Area Two", StartLocation.THREE);
+        locationSelector.addOption("Area Three", StartLocation.THREE);
 
-        actionSelector.setDefaultOption("Score something low and exit", AutoActions.SCORE_LOW);
+        actionSelector.setDefaultOption("Score game piece low and exit", AutoActions.SCORE_LOW);
         actionSelector.addOption("Score cube high and exit", AutoActions.SCORE_CUBE_AND_EXIT);
         actionSelector.addOption("Score cone high and exit", AutoActions.SCORE_CONE_AND_EXIT);
         actionSelector.addOption("Score cone high, exit community, and balance", AutoActions.SCORE_EXIT_BALANCE);
 
-        autoTab.add("Auto Location", locationSelector).withSize(2, 1);
-        autoTab.add("Auto Action", actionSelector).withSize(2, 1);
+        autoTab.add("Auto Location", locationSelector).withSize(2, 1).withPosition(0, 0);
+        autoTab.add("Auto Action", actionSelector).withSize(3, 1).withPosition(2, 0);
+        autoTab.addString("Current Auto: ", () -> getSelectedAuto()).withSize(2, 1).withPosition(0, 1);
+        autoTab.add("Current Auto Trajectory", field).withSize(6, 4).withPosition(2, 1).withProperties(Map.of("colors", "yellow"));
+    }
+
+    private String getSelectedAuto() {
+        return actionSelector.getSelected().value + locationSelector.getSelected().value;
     }
 
     public Command getAutoCommand() {
-        if(actionSelector.getSelected() == AutoActions.SCORE_EXIT_BALANCE) {
-            DriverStation.reportWarning("Auto: Score cone high, exit, and balance", false);
-            return getExitAndBalance(locationSelector.getSelected());
-
-        }else if(actionSelector.getSelected() == AutoActions.SCORE_CONE_AND_EXIT) {
-            DriverStation.reportWarning("Auto: Score cone high and exit", false);
-            return getScoreConeAndExit(locationSelector.getSelected());
-
-        }else if(actionSelector.getSelected() == AutoActions.SCORE_CUBE_AND_EXIT) {
-            DriverStation.reportWarning("Auto: Score cube high and exit", false);
-            return getScoreCubeAndExit(locationSelector.getSelected());
-
-        }else if(actionSelector.getSelected() == AutoActions.SCORE_LOW) {
-            DriverStation.reportWarning("Auto: score something low and exit", false);
-            return getScoreLowAndExit(locationSelector.getSelected());
-        }
-        return null;
+        return builder.fullAuto(PathPlanner.loadPath(getSelectedAuto(), defaulPathConstraints));
     }
 
-    public Command getExitAndBalance(StartLocation startArea) {
-        if(startArea == StartLocation.ONE || startArea == StartLocation.THREE) {
-            PathPlannerTrajectory path = PathPlanner.loadPath("ExitAndBalance A" + startArea.value, defaulPathConstraints);
-            DriverStation.reportWarning("Auto Area: " + startArea.value, false);
-            return builder.fullAuto(path);
-        }else {
-            return null;
-        }
+    /* Called periodically while disabled */
+    public void updateSelectorPose() {
+        PathPlannerTrajectory path = PathPlanner.loadPath(getSelectedAuto(), defaulPathConstraints);
+        field.setRobotPose(path.getInitialPose());
+        field.getObject("traj").setTrajectory(path);
     }
-
-    public Command getScoreConeAndExit(StartLocation startArea) {
-        if(startArea == StartLocation.ONE || startArea == StartLocation.THREE) {
-            PathPlannerTrajectory path = PathPlanner.loadPath("ScoreConeAndExit A" + startArea.value, defaulPathConstraints);
-            DriverStation.reportWarning("Auto Area: " + startArea.value, false);
-            return builder.fullAuto(path);
-        }else {
-            return null;
-        }
-    }
-
-    public Command getScoreCubeAndExit(StartLocation startArea) {
-        if(startArea == StartLocation.ONE || startArea == StartLocation.THREE) {
-            PathPlannerTrajectory path = PathPlanner.loadPath("ScoreCubeAndExit A" + startArea.value, defaulPathConstraints);
-            DriverStation.reportWarning("Auto Area: " + startArea.value, false);
-            return builder.fullAuto(path);
-        }else {
-            return null;
-        }
-    }
-
-    public Command getScoreLowAndExit(StartLocation startArea) {
-        if(startArea == StartLocation.ONE || startArea == StartLocation.THREE) {
-            PathPlannerTrajectory path = PathPlanner.loadPath("ScoreLowAndExit A" + startArea.value, defaulPathConstraints);
-            DriverStation.reportWarning("Auto Area: " + startArea.value, false);
-            return builder.fullAuto(path);
-        }else {
-            return null;
-        }
-    }
-
 }
