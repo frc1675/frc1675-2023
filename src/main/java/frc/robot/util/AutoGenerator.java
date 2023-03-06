7c95e051e0592e01cd83a3299ad1432a13e15048
Jake Herrmann <herrmann.jake@protonmail.com>
2023-03-05 22:15:50 -0600

    Changed auto selection to include start actions rather than each path having a dedicated start action

--- src/main/java/frc/robot/util/AutoGenerator.java
+++ src/main/java/frc/robot/util/AutoGenerator.java
@@ -32,25 +32,37 @@ public class AutoGenerator {
     private SwerveAutoBuilder builder;
 
     private HashMap<String, Command> eventMap = new HashMap<String, Command>();
+    private HashMap<String, Command> startActionMap = new HashMap<String, Command>();
     private PathConstraints defaulPathConstraints = new PathConstraints(Constants.AUTO_MAX_VELOCITY, Constants.AUTO_MAX_ACCELERATION);
 
-    private SendableChooser<AutoActions> actionSelector = new SendableChooser<AutoActions>();
+    private SendableChooser<PathActions> pathActionSelector = new SendableChooser<PathActions>();
+    private SendableChooser<StartActions> startActionSelector = new SendableChooser<StartActions>();
     private SendableChooser<StartLocation> locationSelector = new SendableChooser<StartLocation>();
 
     private ShuffleboardTab autoTab = Shuffleboard.getTab("Auto");
     private Field2d field = new Field2d();
 
-    public enum AutoActions {
+    public enum PathActions {
         SCORE_EXIT_BALANCE("ExitAndBalance"),
-        SCORE_CONE_AND_EXIT("ScoreConeAndExit"),
-        SCORE_CUBE_AND_EXIT("ScoreCubeAndExit"),
-        SCORE_LOW("ScoreLowAndExit");
+        SCORE_AND_EXIT("ScoreAndExit");
 
         public final String value;
-        private AutoActions(String value) {
+        private PathActions(String value) {
             this.value = value + " A";
         }
     } 
+
+    public enum StartActions {
+        SCORE_CUBE_LOW("scoreCubeLow"),
+        SCORE_CONE_LOW("scoreConeLow"),
+        SCORE_CUBE_HIGH("scoreCubeHigh"),
+        SCORE_CONE_HIGH("scoreConeHigh");
+
+        public final String value;
+        private StartActions(String value) {
+            this.value = value;
+        }
+    }
 
     public enum StartLocation {
         ONE(1),
@@ -65,9 +77,11 @@ public class AutoGenerator {
     public AutoGenerator(DrivetrainSubsystem drivetrainSubsystem, FloorArmSubsystem floorArmSubsystem, ArmSubsystem armSubsystem, Intake intake, FloorIntake floorIntake) {   
         SwerveDriveKinematics kinematics = drivetrainSubsystem.getKinematics();
 
-        eventMap.put("scoreLow", new ScoreLow(drivetrainSubsystem, floorArmSubsystem, intake));
-        eventMap.put("scoreConeHigh", new ExtendAndScoreCone(drivetrainSubsystem, floorArmSubsystem, armSubsystem, intake));
-        eventMap.put("scoreCubeHigh", new RotateAndScoreCube(drivetrainSubsystem, floorArmSubsystem, armSubsystem, floorIntake));
+        startActionMap.put("scoreConeLow", new ScoreLow(drivetrainSubsystem, floorArmSubsystem, intake, true));
+        startActionMap.put("scoreCubeLow", new ScoreLow(drivetrainSubsystem, floorArmSubsystem, intake, false));
+        startActionMap.put("scoreConeHigh", new ExtendAndScoreCone(drivetrainSubsystem, floorArmSubsystem, armSubsystem, intake));
+        startActionMap.put("scoreCubeHigh", new RotateAndScoreCube(drivetrainSubsystem, floorArmSubsystem, armSubsystem, floorIntake));
+
         eventMap.put("autoBalance", new PrintCommand("Auto balance begin"));
         eventMap.put("beginCollectCube", new BeginCollectCube(floorArmSubsystem, floorIntake));
         eventMap.put("endCollectCube", new EndCollectCube(floorArmSubsystem, floorIntake));
@@ -87,28 +101,38 @@ public class AutoGenerator {
         locationSelector.setDefaultOption("Area One", StartLocation.ONE); 
         locationSelector.addOption("Area Three", StartLocation.THREE);
 
-        actionSelector.setDefaultOption("Score game piece low and exit", AutoActions.SCORE_LOW);
-        actionSelector.addOption("Score cube high and exit", AutoActions.SCORE_CUBE_AND_EXIT);
-        actionSelector.addOption("Score cone high and exit", AutoActions.SCORE_CONE_AND_EXIT);
-        actionSelector.addOption("Score cone high, exit community, and balance", AutoActions.SCORE_EXIT_BALANCE);
+        pathActionSelector.setDefaultOption("Score game piece and exit", PathActions.SCORE_AND_EXIT);
+        pathActionSelector.addOption("Score cone high, exit community, and balance", PathActions.SCORE_EXIT_BALANCE);
+
+        startActionSelector.setDefaultOption("Score cone low", StartActions.SCORE_CONE_LOW);
+        startActionSelector.addOption("Score cube low", StartActions.SCORE_CUBE_LOW);
+        startActionSelector.addOption("Score cone high", StartActions.SCORE_CONE_HIGH);
+        startActionSelector.addOption("Score cube high", StartActions.SCORE_CUBE_HIGH);
 
         autoTab.add("Auto Location", locationSelector).withSize(2, 1).withPosition(0, 0);
-        autoTab.add("Auto Action", actionSelector).withSize(3, 1).withPosition(2, 0);
-        autoTab.addString("Current Auto: ", () -> getSelectedAuto()).withSize(2, 1).withPosition(0, 1);
+        autoTab.add("Auto Path Action", pathActionSelector).withSize(3, 1).withPosition(2, 0);
+        autoTab.add("Auto Start Action", startActionSelector).withSize(2, 1).withPosition(5, 0);
+        autoTab.addString("Current Auto Path: ", () -> getSelectedPath()).withSize(2, 1).withPosition(0, 1);
+        autoTab.addString("Current Starting Action: ", () -> getSelectedStartAction()).withSize(2, 1).withPosition(0, 2);
         autoTab.add("Current Auto Trajectory", field).withSize(6, 4).withPosition(2, 1).withProperties(Map.of("colors", "yellow"));
     }
 
-    private String getSelectedAuto() {
-        return actionSelector.getSelected().value + locationSelector.getSelected().value;
+    private String getSelectedPath() {
+        return pathActionSelector.getSelected().value + locationSelector.getSelected().value;
     }
 
     public Command getAutoCommand() {
-        return builder.fullAuto(PathPlanner.loadPath(getSelectedAuto(), defaulPathConstraints));
+        return builder.fullAuto(PathPlanner.loadPath(getSelectedPath(), defaulPathConstraints));
+    }
+
+    public String getSelectedStartAction() {
+        eventMap.put("startAction", startActionMap.get(startActionSelector.getSelected().value));
+        return startActionSelector.getSelected().value;
     }
 
     /* Called periodically while disabled */
     public void updateSelectorPose() {
-        PathPlannerTrajectory path = PathPlanner.loadPath(getSelectedAuto(), defaulPathConstraints);
+        PathPlannerTrajectory path = PathPlanner.loadPath(getSelectedPath(), defaulPathConstraints);
         field.setRobotPose(path.getInitialPose());
         field.getObject("traj").setTrajectory(path);
     }
