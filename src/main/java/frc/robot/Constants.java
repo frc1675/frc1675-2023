3beef6340c18fabae6e637e64a1ae8fcaf8bd30b
Golden Experience <ups1675@gmail.com>
2023-03-14 20:16:07 -0500

    New offsets

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -50,22 +50,22 @@ public final class Constants {
       public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
       public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
       public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; 
-      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(40.07);
+      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(71.37);
   
       public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
       public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
       public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10; 
-      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(207.8);
+      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(192.74);
   
       public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; 
       public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
       public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; 
-      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(146.6);
+      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(145.90);
   
       public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
       public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
       public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; 
-      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(289.33);
+      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(288.90);
   
       //controller constants
       public static final int DRIVER_CONTROLLER = 0;
