d7ce52c5ff14ce61ba4e045d676ccb90041fd621
Iron Golem <frc1675@gmail.com>
2023-03-23 12:16:10 -0500

    Tune swerve offset, arm encoder, balance tolerance

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -35,8 +35,8 @@ public final class Constants {
       public static final double SLOW_DRIVE_SCALING = 0.3;
       public static final double SLOW_ROTATION_SCALING = 0.5;
       public static final int INPUT_ROLLING_AVERAGE_SAMPLE_SIZE = 20;
-      public static final double AUTO_BALANCE_TOLERANCE_DEGREES = 1;
-      public static final double MAX_AUTO_BALANCE_TRANSLATION_METERS = 2;
+      public static final double AUTO_BALANCE_TOLERANCE_DEGREES = 3;
+      public static final double MAX_AUTO_BALANCE_TRANSLATION_METERS = 5;
   
       //PID constants
       public static final double ROTATION_PROPORTIONAL_COEFFICENT = 3.0;
@@ -51,22 +51,22 @@ public final class Constants {
       public static final int FRONT_RIGHT_MODULE_DRIVE_MOTOR = 3;
       public static final int FRONT_RIGHT_MODULE_STEER_MOTOR = 4;
       public static final int FRONT_RIGHT_MODULE_STEER_ENCODER = 12; 
-      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(71.37);
+      public static final double FRONT_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(71.28);
   
       public static final int FRONT_LEFT_MODULE_DRIVE_MOTOR = 1;
       public static final int FRONT_LEFT_MODULE_STEER_MOTOR = 2; 
       public static final int FRONT_LEFT_MODULE_STEER_ENCODER = 10; 
-      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(192.74);
-  
+      public static final double FRONT_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(192.68);
+           
       public static final int BACK_RIGHT_MODULE_DRIVE_MOTOR = 7; 
       public static final int BACK_RIGHT_MODULE_STEER_MOTOR = 8; 
       public static final int BACK_RIGHT_MODULE_STEER_ENCODER = 11; 
-      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(145.90);
+      public static final double BACK_RIGHT_MODULE_STEER_OFFSET = -Math.toRadians(146.25);
   
       public static final int BACK_LEFT_MODULE_DRIVE_MOTOR = 5; 
       public static final int BACK_LEFT_MODULE_STEER_MOTOR = 6; 
       public static final int BACK_LEFT_MODULE_STEER_ENCODER = 9; 
-      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(104.24+115);
+      public static final double BACK_LEFT_MODULE_STEER_OFFSET = -Math.toRadians(222.26);
   
       //controller constants
       public static final int DRIVER_CONTROLLER = 0;
@@ -94,12 +94,11 @@ public final class Constants {
       public static final double ARM_P_COEFF = 2.5;
       public static final double ARM_I_COEFF = 0;
       public static final double ARM_D_COEFF = 0;
-      public static final double ARM_SCORE_HIGH_POSITION = 0.3649-.02; 
-      public static final double AUTO_ARM_SCORE_HIGH_POSITION = 0.3749; 
-      public static final double ARM_SCORE_MID_POSITION = 0.4448-.02;
-      //3834 
-      public static final double ARM_HUMAN_PLAYER_POSITION = 0.3834-0.02;
-      public static final double ARM_INSIDE_ROBOT_POSITION = 0.63;
+      public static final double ARM_SCORE_HIGH_POSITION = 0.5522-.02;  
+      public static final double ARM_SCORE_MID_POSITION = 0.6137;
+      
+      public static final double ARM_HUMAN_PLAYER_POSITION = 0.6028- .02;
+      public static final double ARM_INSIDE_ROBOT_POSITION = 0.8068;
       public static final double ARM_MAX_POSITION = 0.0;//fully within robot
       public static final double ARM_MIN_POSITION = 0.0;//completely outside robot
       public static final double ARM_ENCODER_COUNT_ERROR = 1.0;
